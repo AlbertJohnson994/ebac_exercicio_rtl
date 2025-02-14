c00338273681f6bf92767492073b6360026ed834
AlbertJohnson994 <112587686+AlbertJohnson994@users.noreply.github.com>
2025-02-14 16:09:15 -0300

    Adding up testIds for testing

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import PostComments from '../PostComments';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+
+
+describe('Teste para o componente PostComments', () => {
+    it('Deve adicionar dois comentários', async () => {
+        render(<PostComments />);
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const button = screen.getByTestId('comment-button');
+
+        await userEvent.type(textarea, 'Primeiro comentário');
+        await userEvent.click(button);
+        await screen.findByText('Primeiro comentário'); // Garante que está no DOM
+
+        await userEvent.type(textarea, 'Segundo comentário');
+        await userEvent.click(button);
+        await screen.findByText('Segundo comentário');
+
+        const comments = screen.getAllByTestId('comment-item');
+        expect(comments).toHaveLength(2);
     });
 });
